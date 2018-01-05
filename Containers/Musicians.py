0096b08094ee4870965ba05a94c674a4ae9ae010
tomgonzo <tomas.gonzo@gmail.com>
2018-01-05 12:30:20 -0500

    Adding Challenge 1
    Added list of musicians, practicing append

---
+++ Containers/Musicians.py
@@ -0,0 +1,11 @@
+#List of Favorite Musicians - Challenge 1
+
+musicians = list()
+
+rock = ("Pink Floyd", "ACDC", "Dream Theater")
+electronic = ("Infected Mushroom", "DJ Tiesto", "Mandragora")
+
+musicians.append(rock)
+musicians.append(electronic)
+
+print(musicians)
